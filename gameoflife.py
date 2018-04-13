1a859286d878076eb0cd1383ea65a04124132461
semyon-san <semyon.h@yandex.com>
2018-04-14 01:18:47 +0300

    optimized - just one grid and changing cells

--- gameoflife.py
+++ gameoflife.py
@@ -10,20 +10,25 @@ class Cell(object):
     CELL_LIVE = '#'
     CELL_DEAD = ' '
 
-    def __init__(self, is_live, row, col):
+    def __init__(self, row, col, is_live=True):
         self._live = is_live
+        self._future_live = is_live
+        self._last_processed_time = 0
+
         self._processed = False
+
         self._row = row
         self._col = col
+
 
     def __str__(self):
         return Cell.CELL_LIVE if self._live else Cell.CELL_DEAD
 
     def live(self):
-        self._live = True
+        self._future_live = True
 
     def die(self):
-        self._live = False
+        self._future_live = False
 
     def is_live(self):
         return self._live
@@ -34,11 +39,17 @@ class Cell(object):
     def is_processed(self):
         return self._processed
 
-    def processed(self):
+    def processed(self, clock):
         self._processed = True
+        self._last_processed_time = clock
 
-    def unprocessed(self):
-        self._processed = False
+    def sync(self, clock):
+        if (clock != self._last_processed_time):
+            self._processed = False
+            self._last_processed_time = clock
+
+    def change_state(self):
+        self._live = self._future_live
 
     @property
     def row(self):
@@ -53,27 +64,28 @@ class GameOfLife(object):
         self._grid_rows = rows
         self._grid_cols = cols
 
-        self._current_live_cells = []
-        self._future_live_cells = []
+        self._live_cells = {}
 
-        self._processed_cells = []
+        self._changed_cells = []
 
-        self._current_grid = self._make_grid(rows, cols, live_cells_coords)
-        self._future_grid = self._make_grid(rows, cols, [])
+        self._clock = 0
+
+        self._grid = self._make_grid(rows, cols, live_cells_coords)
 
     def num_live_cells(self):
-        return len(self._current_live_cells)
+        return len(self._live_cells)
 
     def tick(self):
-        for live_cell in self._current_live_cells:
-            row = live_cell.row
-            col = live_cell.col
+        for live_cell in self._live_cells:
+            cell_y = live_cell.row
+            cell_x = live_cell.col
 
             # processing the live cell and adjacent cells
-            for r in range(row-1, row+1+1):
-                for c in range(col-1, col+1+1):
-                    if ((r >= 0) and (r < self._grid_rows)) and ((c >= 0) and (c < self._grid_cols)):
-                        cell = self._current_grid[r][c]
+            for y in range(cell_y-1, cell_y+1+1):
+                for x in range(cell_x-1, cell_x+1+1):
+                    if ((y >= 0) and (y < self._grid_rows)) and ((x >= 0) and (x < self._grid_cols)):
+                        cell = self._grid[y][x]
+                        cell.sync(self._clock)
                         if not cell.is_processed():
                             self._process(cell)
 
@@ -81,7 +93,7 @@ class GameOfLife(object):
 
     @property
     def grid(self):
-        return self._current_grid
+        return self._grid
 
     @property
     def rows(self):
@@ -92,27 +104,16 @@ class GameOfLife(object):
         return self._grid_cols
 
     def _process(self, cell):
-        current_cell = cell
-        future_cell = self._future_grid[current_cell.row][current_cell.col]
-
-        n = self._num_live_neighbours(current_cell)
-        if current_cell.is_live():
-            if n < 2:
-                future_cell.die() # solitude
-            elif n > 3:
-                future_cell.die() # overpopulation
-            else:
-                future_cell.live() # 3 is good
-                self._future_live_cells.append(future_cell)
+        num_live_neighbours = self._num_live_neighbours(cell)
+        if cell.is_live():
+            if num_live_neighbours < 2 or num_live_neighbours > 3:
+                cell.die() # solitude or overpopulation
+                self._changed_cells.append(cell)
         else:
-            if n == 3:
-                future_cell.live() # 3 is good
-                self._future_live_cells.append(future_cell)
-            else:
-                future_cell.die() # remain dead
-
-        current_cell.processed()
-        self._processed_cells.append(current_cell)
+            if num_live_neighbours == 3:
+                cell.live() # 3 is good
+                self._changed_cells.append(cell)
+        cell.processed(self._clock)
 
     def _num_live_neighbours(self, cell):
         n = 0
@@ -120,28 +121,33 @@ class GameOfLife(object):
             for c in range(cell.col-1, cell.col+1+1):
                 if not (r == cell.row and c == cell.col):
                     if ((r >= 0) and (r < self._grid_rows)) and ((c >= 0) and (c < self._grid_cols)):
-                        if self._current_grid[r][c].is_live():
+                        if self._grid[r][c].is_live():
                             n += 1
         return n
 
+    def _update_clock(self):
+        self._clock += 1
+
     def _progress(self):
-        self._current_grid = self._future_grid 
-        self._future_grid = self._make_grid(self._grid_rows, self._grid_cols, [])
+        for cell in self._changed_cells:
+            cell.change_state()
+            if cell.is_live():
+                self._live_cells[cell] = cell
+            else:
+                self._live_cells.pop(cell)
 
-        self._current_live_cells = self._future_live_cells
-        self._future_live_cells = []
+        self._changed_cells = []
 
-        for cell in self._processed_cells:
-            cell.unprocessed()
-        self._processed_cells = []
+        self._update_clock()
 
     def _make_grid(self, grid_rows, grid_cols, live_cells_coords):
-        grid = [[Cell(False, row=r, col=c) for c in range(grid_cols)] for r in range(grid_rows)]
-        
+        grid = [[Cell(r, c, is_live=False) for c in range(grid_cols)] for r in range(grid_rows)]
+
         for row, col in live_cells_coords:
             cell = grid[row][col]
             cell.live()
-            self._current_live_cells.append(cell)
+            cell.change_state()
+            self._live_cells[cell] = cell
 
         return grid
 
